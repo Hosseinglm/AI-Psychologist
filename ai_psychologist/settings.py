3da49a480d449b553a27fc2fcfe7f362a17e403d
root <hossein.glmm@gmail.com>
2025-06-05 13:43:05 +0000

    setting file fixed

--- ai_psychologist/settings.py
+++ ai_psychologist/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'localhost:8003', '127.0.0.1:8003', '0.0.0.0:8003']
+ALLOWED_HOSTS = ['aipsychologist.hprojects.dev','localhost', '127.0.0.1']
 
 
 # Application definition
